4efe33f80f4b9985c5ce1bd83cc577fc5b8951b4
N.Pantiukhova <NataliaPantiukhova@users.noreply.github.com>
2020-10-07 20:07:14 +0300

    feat: add one more resolved test

--- src/transform-array.js
+++ src/transform-array.js
@@ -4,37 +4,40 @@ module.exports = function transform(arr) {
   let resultArray = [],
       arrayLength = arr.length,
       filteredArray = [];
-
+ console.log(arr);
+ const processedValue = 'xxx';
   for (let i = 0; i < arrayLength; i++){
     switch (arr[i]) {
       case '--discard-next':
         if ((i + 1) < arrayLength){
-          resultArray.splice(0, i);
-          resultArray.splice(0, i + 1);
+          resultArray[i] = processedValue;
+          resultArray[i + 1] = processedValue;
         } else {
-          resultArray.splice(0, i);
+          resultArray[i] = processedValue;
         };
         break;
       case '--discard-prev':
         if ((i - 1) >= 0){
-          resultArray.splice(0, i);
-          resultArray.splice(0, i - 1);
+          resultArray[i] = processedValue;
+          resultArray[i - 1] = processedValue;
         } else {
-          resultArray.splice(0, i);
+          resultArray[i] = processedValue;
         };
         break;
       case '--double-next':
         if ((i + 1) < arrayLength){
-          resultArray[i] = arr[i + 1];
+          resultArray[i] = processedValue;
+          resultArray[i + 1] = arr[i + 1];
         } else {
-          resultArray.splice(0, i);
+          resultArray[i] = processedValue;
         };
         break;
       case '--double-prev':
         if ((i - 1) >= 0){
-          resultArray[i] = arr[i - 1];
+          resultArray[i] = processedValue;
+          resultArray[i - 1] = arr[i - 1];
         } else {
-          resultArray.splice(0, i);
+          resultArray[i] = processedValue;
         };
         break;
       default:
@@ -43,23 +46,6 @@ module.exports = function transform(arr) {
     }
   };
   
-// console.log(arr);
-//console.log(resultArray);
-  filteredArray = resultArray.filter(function () { return true });
-  //console.log(resultArray);
-  filteredArray = resultArray;
-  filteredArray = resultArray.filter(function( element ) {
-    // console.log(typeof element);
-    // return element !== undefined && element != null;
-
-      // return element !== null;
-      return true;
-  });
-  for (let i = 0; i < filteredArray.length; i++){
-    if (typeof filteredArray[i] == 'undefined' ){
-      filteredArray.splice(0, i);
-    };
-  };
-  filteredArray = resultArray;//filteredArray.filter(function () { return true });
+  filteredArray = resultArray.filter(item => item !== processedValue);
   return filteredArray;
 };
